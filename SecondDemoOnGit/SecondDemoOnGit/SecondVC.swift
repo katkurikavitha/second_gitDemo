415e85d66c675efb57ffecde5636dbb51e87cdb4
katkurikavitha <kavitha.katkuri@appitventures.com>
2019-02-01 13:46:45 +0530

    added vc

---
+++ SecondDemoOnGit/SecondDemoOnGit/SecondVC.swift
@@ -0,0 +1,30 @@
+//
+//  SecondVC.swift
+//  SecondDemoOnGit
+//
+//  Created by Malathi on 2/1/19.
+//  Copyright © 2019 Appitventures. All rights reserved.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   var addedVC = UIView()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
