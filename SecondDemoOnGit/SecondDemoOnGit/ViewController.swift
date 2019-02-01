9ac770cd27d4339a28bf92e6963739f3a4947162
katkurikavitha <kavitha.katkuri@appitventures.com>
2019-02-01 12:47:25 +0530

    second commit by rashida

--- SecondDemoOnGit/SecondDemoOnGit/ViewController.swift
+++ SecondDemoOnGit/SecondDemoOnGit/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 //first commit by kavitha
+    //second commit by rashida
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
